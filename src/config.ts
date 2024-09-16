b85a9787d203357c4b684f95a812af96c0954b83
danielsanchez68 <danielsanchez68@hotmail.com>
2024-09-16 09:48:48 -0300

    v0.0.7.0.1

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const PORT = process.env.PORT || 8080
+const PORT = Number(process.env.PORT || 8080) + 1
 const HOST_EXT = process.env.HOST_EXT || '127.0.0.1'
 const PORT_EXT = process.env.PORT_EXT || 8081
 
